09bfafe707fd8390d2b7e2683ba5524c126ca85b
Yuuri <109029219+gayulz@users.noreply.github.com>
2024-02-10 00:52:47 +0900

    today : 회원가입 구현중

--- src/main/java/jobis/jobisimpTask/utils/SwaggerConfig.java
+++ src/main/java/jobis/jobisimpTask/utils/SwaggerConfig.java
@@ -9,27 +9,27 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@Configuration
-@EnableSwagger2
+//
+//@Configuration
+//@EnableSwagger2
 public class SwaggerConfig {
-    @Bean
-    public Docket restAPI(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("j2kb.ponicon.scrap")) // 특정 패키지경로를 API문서화 한다.
-                .paths(PathSelectors.any()) // apis중에서 특정 path조건 API만 문서화 하는 2차 필터
-                .build()
-                .groupName("API 1.0.0") // group별 명칭을 주어야 한다.
-                .useDefaultResponseMessages(false); // 400,404,500 .. 표기를 ui에서 삭제한다.
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Spring Boot REST API test")
-                .version("v0.0.1")
-                .description("jobis-impTask swagger api 입니다.")
-                .build();
-    }
+//    @Bean
+//    public Docket restAPI(){
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("j2kb.ponicon.scrap")) // 특정 패키지경로 API문서화
+//                .paths(PathSelectors.any()) // apis중에서 특정 path조건 API만 문서화 2차 필터
+//                .build()
+//                .groupName("API 1.0.0") // group별 명칭
+//                .useDefaultResponseMessages(false); // 400,404,500 .. ui에서 삭제
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("Spring Boot REST API test")
+//                .version("v0.0.1")
+//                .description("jobis-impTask swagger api 입니다.")
+//                .build();
+//    }
 }
