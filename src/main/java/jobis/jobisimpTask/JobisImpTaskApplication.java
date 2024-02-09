5025df907e29a684bc15494ceeb80af8bfecf8de
Yuuri <109029219+gayulz@users.noreply.github.com>
2024-02-09 15:11:58 +0900

    과제 시작-환경구성

---
+++ src/main/java/jobis/jobisimpTask/JobisImpTaskApplication.java
@@ -0,0 +1,13 @@
+package jobis.jobisimpTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobisImpTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobisImpTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jobis/jobisimpTask/JobisImpTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package jobis.jobisimpTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobisImpTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
